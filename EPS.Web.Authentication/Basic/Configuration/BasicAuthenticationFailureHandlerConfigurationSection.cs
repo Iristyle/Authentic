8149074c21e7c867a44ed0b2d93b0a0b1717dd72
ebrown <devnull@localhost>
2010-12-30 15:18:43 -0500

    added docs

--- EPS.Web.Authentication/Basic/Configuration/BasicAuthenticationFailureHandlerConfigurationSection.cs
+++ EPS.Web.Authentication/Basic/Configuration/BasicAuthenticationFailureHandlerConfigurationSection.cs
@@ -4,8 +4,12 @@ using EPS.Web.Authentication.Configuration;
 
 namespace EPS.Web.Authentication.Basic.Configuration
 {
+    /// <summary>   Basic authentication failure handler configuration section. </summary>
+    /// <remarks>   ebrown, 12/30/2010. </remarks>
     public class BasicAuthenticationFailureHandlerConfigurationSection : HttpContextInspectingAuthenticationFailureConfigurationSection
     {
+        /// <summary>   Gets or sets the realm of the cookie on an outgoing cookie request. </summary>
+        /// <value> The realm. </value>
         [ConfigurationProperty("realm", DefaultValue = "localhost")]
         [StringValidator(MinLength = 1)]
         public string Realm
