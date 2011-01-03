ef063f8fe62051d208070fc38afabcf6dbcd5d62
ebrown <devnull@localhost>
2011-01-03 16:23:11 -0500

    added docs

--- EPS.Web.Authentication/Abstractions/IHttpContextInspectingAuthenticatorFactory.cs
+++ EPS.Web.Authentication/Abstractions/IHttpContextInspectingAuthenticatorFactory.cs
@@ -3,14 +3,27 @@ using EPS.Web.Authentication.Configuration;
 
 namespace EPS.Web.Authentication.Abstractions
 {
+    /// <summary>   Factory interface for constructing a specific type of http context inspecting authenticator. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public interface IHttpContextInspectingAuthenticatorFactory
     {
+        /// <summary>   Constructs the http context inspecting authenticator. </summary>
+        /// <param name="config">   The configuration. </param>
+        /// <returns>   An authenticator instance. </returns>
         IHttpContextInspectingAuthenticator Construct(HttpContextInspectingAuthenticatorConfigurationElement config);
     }
 
+    /// <summary>   
+    /// Generic factory interface for constructing a specific type of http context inspecting authenticator, given a more specialized
+    /// configuration type. 
+    /// </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public interface IHttpContextInspectingAuthenticatorFactory<T> : IHttpContextInspectingAuthenticatorFactory
         where T: HttpContextInspectingAuthenticatorConfigurationElement
     {
+        /// <summary>   Constructs the http context inspecting authenticator. </summary>
+        /// <param name="config">   The specialized configuration as specified in the class definition. </param>
+        /// <returns>   An authenticator instance. </returns>
         IHttpContextInspectingAuthenticator<T> Construct(T config);
     }
 }
