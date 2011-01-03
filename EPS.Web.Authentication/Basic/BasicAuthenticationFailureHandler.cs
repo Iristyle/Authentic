c044eb01bfeecd4a12714a4d1198469f1141df63
ebrown <devnull@localhost>
2011-01-03 16:23:41 -0500

    fixed FxCop globalization issues reworked constructor added docs

--- EPS.Web.Authentication/Basic/BasicAuthenticationFailureHandler.cs
+++ EPS.Web.Authentication/Basic/BasicAuthenticationFailureHandler.cs
@@ -1,31 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Net;
 using System.Security.Principal;
 using System.Web;
-using EPS.Web.Authentication.Configuration;
 using EPS.Web.Authentication.Abstractions;
 using EPS.Web.Authentication.Basic.Configuration;
 
 namespace EPS.Web.Authentication.Basic
 {
+    /// <summary>   A failure handler that sends out a basic authentication WWW-Authenticate header if authentication fails. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class BasicAuthenticationFailureHandler : 
         HttpContextInspectingAuthenticationFailureHandlerBase<BasicAuthenticationFailureHandlerConfigurationSection>
     {
-        internal BasicAuthenticationFailureHandler(BasicAuthenticationFailureHandlerConfigurationSection config)
+        /// <summary>   Constructs an instance of the handler given configuration values. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration. </param>
+        public BasicAuthenticationFailureHandler(BasicAuthenticationFailureHandlerConfigurationSection config)
             : base(config) {}
 
         #region IHttpHeaderInspectingAuthenticationFailureHandler Members
+        /// <summary>   
+        /// Executes the authentication failure action, sending a WWW-Authenticate header with the configured realm out through the context and
+        /// setting the HTTP status code to 401 (Unauthorized). 
+        /// </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="context">          The incoming context. </param>
+        /// <param name="inspectorResults"> The set of failed inspector results. </param>
+        /// <returns>   Null -- no IPrincipal is returned as the response is completed after sending the authenticate header. </returns>
         public override IPrincipal OnAuthenticationFailure(HttpContextBase context, Dictionary<IHttpContextInspectingAuthenticator, InspectorAuthenticationResult> inspectorResults)
         {
             context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-            context.Response.AddHeader("WWW-Authenticate", String.Format("Basic realm=\"{0}\"", Configuration.Realm));
+            context.Response.AddHeader("WWW-Authenticate", String.Format(CultureInfo.InvariantCulture, "Basic realm=\"{0}\"", Configuration.Realm));
             context.ApplicationInstance.CompleteRequest();
 
             return null;
         }
-
         #endregion
     }
-
 }
