56c661cca42d42486f8b52b83faac5ec201c064e
ebrown <devnull@localhost>
2011-01-03 16:23:48 -0500

    added docs

--- EPS.Web.Authentication/Basic/BasicAuthenticationFailureHandlerFactory.cs
+++ EPS.Web.Authentication/Basic/BasicAuthenticationFailureHandlerFactory.cs
@@ -5,10 +5,21 @@ using EPS.Web.Authentication.Abstractions;
 
 namespace EPS.Web.Authentication.Basic
 {
+    /// <summary>   An implementation of a simple Basic authentication failure handler factory. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class BasicAuthenticationFailureHandlerFactory : 
         HttpContextInspectingAuthenticationFailureHandlerFactoryBase<BasicAuthenticationFailureHandlerConfigurationSection>
     {
         #region IHttpHeaderInspectingAuthenticationFailureHandlerFactory Members
+        /// <summary>   
+        /// Constructs a new instance of a <see cref="T:EPS.Web.Authentication.Basic.BasicAuthenticationFailureHandler"/>, returning a
+        /// <see cref="T:
+        /// EPS.Web.Authentication.Abstractions.IHttpContextInspectingAuthenticationFailureHandler{BasicAuthenticationFailureHandlerConfigurationSe
+        /// ction}"/> . 
+        /// </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration. </param>
+        /// <returns>   A new failure handler instance. </returns>
         public override IHttpContextInspectingAuthenticationFailureHandler<BasicAuthenticationFailureHandlerConfigurationSection> Construct(BasicAuthenticationFailureHandlerConfigurationSection config)
         {
             return new BasicAuthenticationFailureHandler(config);
