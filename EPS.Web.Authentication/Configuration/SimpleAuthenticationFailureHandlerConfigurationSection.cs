f5f619fa4d4f60ce8faf6d5cd1bcb3dc144a4784
ebrown <devnull@localhost>
2011-01-03 16:30:50 -0500

    added docs

--- EPS.Web.Authentication/Configuration/SimpleAuthenticationFailureHandlerConfigurationSection.cs
+++ EPS.Web.Authentication/Configuration/SimpleAuthenticationFailureHandlerConfigurationSection.cs
@@ -3,6 +3,8 @@ using EPS.Web.Authentication;
 
 namespace EPS.Web.Authentication.Configuration
 {
+    /// <summary>   Simple authentication failure handler configuration section.  Empty as there is no configuration. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class SimpleAuthenticationFailureHandlerConfigurationSection : HttpContextInspectingAuthenticationFailureConfigurationSection
     { }
 }
