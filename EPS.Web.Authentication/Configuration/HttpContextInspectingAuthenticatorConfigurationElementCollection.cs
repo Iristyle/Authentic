7b589497b195f5a40618597725148c0a8126129b
ebrown <devnull@localhost>
2011-01-03 16:35:52 -0500

    added docs added FxCop globalization fixes

--- EPS.Web.Authentication/Configuration/HttpContextInspectingAuthenticatorConfigurationElementCollection.cs
+++ EPS.Web.Authentication/Configuration/HttpContextInspectingAuthenticatorConfigurationElementCollection.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.Xml;
 using EPS.Configuration;
@@ -10,21 +11,34 @@ using EPS.Web.Authentication.Abstractions;
 namespace EPS.Web.Authentication.Configuration
 {
     //http://www.codeproject.com/KB/dotnet/mysteriesofconfiguration3.aspx
+    /// <summary>   Collection of http context inspecting authenticator configuration elements. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class HttpContextInspectingAuthenticatorConfigurationElementCollection : 
         ConfigurationElementCollection<string, HttpContextInspectingAuthenticatorConfigurationElement>
     {
-        private static Dictionary<string, HttpContextInspectingAuthenticatorConfigurationElement> derivedElements = new Dictionary<string, HttpContextInspectingAuthenticatorConfigurationElement>();
+        private readonly static Dictionary<string, HttpContextInspectingAuthenticatorConfigurationElement> derivedElements 
+            = new Dictionary<string, HttpContextInspectingAuthenticatorConfigurationElement>();
 
+        /// <summary>   Gets the type of the collection -- and AddRemoveClearMap. </summary>
+        /// <value> The type of the collection. </value>
         public override ConfigurationElementCollectionType CollectionType
         {
             get { return ConfigurationElementCollectionType.AddRemoveClearMap; }
         }
 
+        /// <summary>   Gets the name of the config element - "inspectors". </summary>
+        /// <value> "inspectors" - the name of the element. </value>
         protected override string ElementName
         {
             get { return "inspectors"; }
         }
 
+        /// <summary>   Executes the deserialize unrecognized element action. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <exception cref="ConfigurationErrorsException"> Thrown when the factory types specified are not correct or cannot be instantiated. </exception>
+        /// <param name="elementName">  Name of the element. </param>
+        /// <param name="reader">       The reader. </param>
+        /// <returns>   true if it succeeds, false if it fails. </returns>
         protected override bool OnDeserializeUnrecognizedElement(string elementName, XmlReader reader)
         {
             //verify that this element is OK and has a valid factory spec'd
@@ -33,15 +47,15 @@ namespace EPS.Web.Authentication.Configuration
                 string factoryTypeName = reader.GetAttribute("factory");
                 Type factoryType = Type.GetType(factoryTypeName, true, true);
                 if (null == factoryType)
-                    throw new ConfigurationErrorsException(String.Format("The factory type specified [{0}] cannot be found - check configuration settings", factoryTypeName ?? string.Empty));
+                    throw new ConfigurationErrorsException(String.Format(CultureInfo.CurrentCulture, "The factory type specified [{0}] cannot be found - check configuration settings", factoryTypeName ?? string.Empty));
 
                 var genericTypeParameter = typeof(IHttpContextInspectingAuthenticatorFactory<>).GetGenericInterfaceTypeParameters(factoryType).ToList();
                 if (genericTypeParameter.Count == 0)
-                    throw new ConfigurationErrorsException(String.Format("The factory type specified [{0}] must implement interface {1} - check configuration settings", factoryTypeName ?? string.Empty, typeof(IHttpContextInspectingAuthenticatorFactory<>).Name));
+                    throw new ConfigurationErrorsException(String.Format(CultureInfo.CurrentCulture, "The factory type specified [{0}] must implement interface {1} - check configuration settings", factoryTypeName ?? string.Empty, typeof(IHttpContextInspectingAuthenticatorFactory<>).Name));
 
                 //this automatically throws when there's no default parameterless constructor
                 //just create an instance and see what happens ;0
-                var testInstance = Activator.CreateInstance(factoryType);
+                Activator.CreateInstance(factoryType);
 
                 //Step 1 -- create the *real* configuration elementName type we need here, caching it, 
                 //let the config system deserialize the same xml chunk
@@ -58,6 +72,10 @@ namespace EPS.Web.Authentication.Configuration
         }
 
         //HACK: this is really hacky, and there might be another way, but the config system is *weird*
+        /// <summary>   Creates a new element. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="elementName">  Name of the element. </param>
+        /// <returns>   An existing element if one exists with the given named, or else calls base class CreateNewElement. </returns>
         protected override ConfigurationElement CreateNewElement(string elementName)
         {
             //Step 2 -- config system recreates elements and rebuilds the collection for some reason
@@ -68,15 +86,13 @@ namespace EPS.Web.Authentication.Configuration
             return base.CreateNewElement(elementName);
         }
 
+        /// <summary>   Gets the elements key, in this case, its name. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="element">  The configuration element. </param>
+        /// <returns>   The element key. </returns>
         public override string GetElementKey(HttpContextInspectingAuthenticatorConfigurationElement element)
         {
             return element.Name;
-        }
-
-        //use the specified type names to create actual inspector instances
-        public IEnumerable<IHttpContextInspectingAuthenticator> GetInspectors()
-        {
-            return this.OfType<HttpContextInspectingAuthenticatorConfigurationElement>().Select(i => i.GetInspector());
-        }
+        }        
     }
 }
