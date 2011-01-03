6b6e01a0cdcac79ba6f598c470cc1edd59fc3160
ebrown <devnull@localhost>
2011-01-03 16:26:26 -0500

    added docs

--- EPS.Web.Authentication/RedirectOnAuthenticationFailureHandlerFactory.cs
+++ EPS.Web.Authentication/RedirectOnAuthenticationFailureHandlerFactory.cs
@@ -4,12 +4,21 @@ using EPS.Web.Authentication.Abstractions;
 
 namespace EPS.Web.Authentication
 {
-    //would like to just use HttpHeaderInspectingAuthenticationFailureConfigurationSection here, but compiler won't let us
-    //so we have to cook up a renamed version of the class and call it AuthenticationFailureRedirectHandlerConfigurationSection
+    /// <summary>   Redirect on authentication failure handler factory. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class RedirectOnAuthenticationFailureHandlerFactory :
             HttpContextInspectingAuthenticationFailureHandlerFactoryBase<RedirectOnAuthenticationFailureHandlerConfigurationSection>
     {
         #region IHttpHeaderInspectingAuthenticationFailureHandlerFactory Members
+        /// <summary>   
+        /// Constructs a new instance of a <see cref="T:EPS.Web.Authentication.RedirectOnAuthenticationFailureHandler"/>, returning it as a <see
+        /// cref="T:
+        /// EPS.Web.Authentication.Abstractions.IHttpContextInspectingAuthenticationFailureHandler{RedirectOnAuthenticationFailureHandlerConfigurat
+        /// ionSection}"/> . 
+        /// </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration. </param>
+        /// <returns>   A redirect on failure handler. </returns>
         public override IHttpContextInspectingAuthenticationFailureHandler<RedirectOnAuthenticationFailureHandlerConfigurationSection> Construct(
             RedirectOnAuthenticationFailureHandlerConfigurationSection config)
         {
