68c30cf5e5e5b9e64b41c91f172f665b546b5966
ebrown <devnull@localhost>
2011-01-03 16:24:37 -0500

    fixed FxCop globalization issues reworked constructor added docs changed implementation of MembershipProvider property to method

--- EPS.Web.Authentication/Basic/BasicAuthenticationInspectingAuthenticator.cs
+++ EPS.Web.Authentication/Basic/BasicAuthenticationInspectingAuthenticator.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Configuration;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Net;
 using System.Security.Principal;
 using System.Web;
@@ -11,17 +12,29 @@ using EPS.Web.Management;
 
 namespace EPS.Web.Authentication.Basic
 {
-    public class BasicAuthenticationInspectingAuthenticator : HttpContextInspectingAuthenticatorBase<BasicAuthenticationHeaderInspectorConfigurationElement>
+    /// <summary>   
+    /// Basic authentication inspecting authenticator.  Inspects HTTP 'Authorization' headers and generates IPrincipals based on the configured
+    /// MembershipProvider. 
+    /// </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
+    public class BasicAuthenticationInspectingAuthenticator : 
+        HttpContextInspectingAuthenticatorBase<BasicAuthenticationHeaderInspectorConfigurationElement>
     {
-        internal BasicAuthenticationInspectingAuthenticator(BasicAuthenticationHeaderInspectorConfigurationElement config) 
+        /// <summary>   Constructs an instance of a basic authenticator. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration. </param>
+        public BasicAuthenticationInspectingAuthenticator(BasicAuthenticationHeaderInspectorConfigurationElement config) 
             : base(config) {}
-
+        
+        /// <summary>   Authenticates a HttpContextBase given a specified MembershipProvider. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="context">  The context. </param>
+        /// <returns>   A success or failure if the MembershipProvider validated the credentials found in the header. </returns>
         public override InspectorAuthenticationResult Authenticate(HttpContextBase context)
         {            
             string authHeader = context.Request.Headers["Authorization"];
-
-            log.InfoFormat("Authorization header [{0}] received", 
-                authHeader.IfMissing("**Missing**"));
+            
+            Log.InfoFormat(CultureInfo.InvariantCulture, "Authorization header [{0}] received", authHeader.IfMissing("**Missing**"));
 
             try
             {
@@ -29,29 +42,31 @@ namespace EPS.Web.Authentication.Basic
                 NetworkCredential credentials;
                 if (!HttpBasicAuthHeaderParser.TryExtractCredentialsFromHeader(authHeader, out credentials))
                     //don't log an event since there was nothing to succeed / fail
-                    return new InspectorAuthenticationResult() { Success = false, Principal = null };
+                    return new InspectorAuthenticationResult(false, null, "No basic credentials found in HTTP header");
+
+                var membershipProvider = GetMembershipProvider();
 
                 //either we don't need to validate, or the user specified a validator
-                if (null == MembershipProvider || MembershipProvider.ValidateUser(credentials.UserName, credentials.Password))
+                if (null == membershipProvider || membershipProvider.ValidateUser(credentials.UserName, credentials.Password))
                 {
-                    if (null != MembershipProvider)
+                    if (null != membershipProvider)
                         new AuthenticationSuccessEvent(this, credentials.UserName).Raise();
 
                     IPrincipal principal = GetPrincipal(context, credentials.UserName, credentials.Password);
                     if (null != principal && null != principal.Identity && principal.Identity.IsAuthenticated)
                     {
                         new AuthenticationSuccessEvent(this, principal.Identity.Name).Raise();
-                        return new InspectorAuthenticationResult() { Success = true, Principal = principal };
+                        return new InspectorAuthenticationResult(true, principal, string.Empty);
                     }
                 }
 
                 new AuthenticationFailureEvent(this, credentials.UserName).Raise();
-                return new InspectorAuthenticationResult() { Success = false, Principal = null };
+                return new InspectorAuthenticationResult(false, null, string.Empty);
             }
             catch (Exception)
             {
                 new AuthenticationFailureEvent(this, string.Empty).Raise();
-                return null;
+                throw;
             }
         }
 
@@ -72,28 +87,31 @@ namespace EPS.Web.Authentication.Basic
             return new GenericPrincipal(id, null);
         }
 
-        public MembershipProvider MembershipProvider
+        /// <summary>   Gets the membership provider defined in configuration, or null if not specified. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <exception cref="ArgumentOutOfRangeException">  Thrown when one or more arguments are outside the required range. </exception>
+        /// <returns>   The membership provider that is used to validate incoming credentials. </returns>
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "This is not suitable for a property as configuration is inspected and exceptions may be thrown")]
+        public MembershipProvider GetMembershipProvider()
         {
-            get
+            if (string.IsNullOrWhiteSpace(Configuration.ProviderName))
             {
-                if (string.IsNullOrWhiteSpace(Configuration.ProviderName))
-                    return null;
+                return null;
+            }
 
-                if (string.Equals(Configuration.ProviderName, "default", StringComparison.OrdinalIgnoreCase))
-                {
-                    MembershipProvider currentProvider = Membership.Provider;
-                    log.InfoFormat("Default provider of [{0}] selected", (null != currentProvider ? currentProvider.Name.IfMissing("N/A") : "N/A"));
-                    return currentProvider;
-                }
-                else
-                {
-                    MembershipProvider provider = Membership.Providers[Configuration.ProviderName];
-                    if (provider == null)
-                        throw new ConfigurationErrorsException(String.Format("Provider {0} not found", Configuration.ProviderName));
-
-                    log.InfoFormat("Custom provider of [{0}] selected", provider.Name.IfMissing("*No Name*"));
-                    return provider;
-                }
+            if (string.Equals(Configuration.ProviderName, "default", StringComparison.OrdinalIgnoreCase))
+            {
+                MembershipProvider currentProvider = Membership.Provider;
+                Log.InfoFormat(CultureInfo.InvariantCulture, "Default provider of [{0}] selected", (null != currentProvider ? currentProvider.Name.IfMissing("N/A") : "N/A"));
+                return currentProvider;
+            }
+            else
+            {
+                MembershipProvider provider = Membership.Providers[Configuration.ProviderName];
+                if (provider == null)
+                    throw new ArgumentOutOfRangeException(String.Format(CultureInfo.InvariantCulture, "Provider {0} specified in configuration not found", Configuration.ProviderName));
+                Log.InfoFormat(CultureInfo.InvariantCulture, "Custom provider of [{0}] specified in configuration selected", provider.Name.IfMissing("*No Name*"));
+                return provider;
             }
         }
     }
