fc680f6acd78d761ebb3c5f64d7d06a34e23e448
ebrown <devnull@localhost>
2011-01-03 16:26:39 -0500

    added docs reworked constructor accessibility

--- EPS.Web.Authentication/RedirectOnAuthenticationFailureHandler.cs
+++ EPS.Web.Authentication/RedirectOnAuthenticationFailureHandler.cs
@@ -2,19 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Security.Principal;
 using System.Web;
-using EPS.Web.Authentication.Configuration;
 using EPS.Web.Authentication.Abstractions;
-
+using EPS.Web.Authentication.Configuration;
 
 namespace EPS.Web.Authentication
 {
+    /// <summary>   A very simple authentication failure handler that redirects to a preconfigured url. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class RedirectOnAuthenticationFailureHandler :
            HttpContextInspectingAuthenticationFailureHandlerBase<RedirectOnAuthenticationFailureHandlerConfigurationSection>
     {
-        internal RedirectOnAuthenticationFailureHandler(RedirectOnAuthenticationFailureHandlerConfigurationSection config)
+        /// <summary>   Constructs an instance of a RedirectOnAuthenticationFailureHandler. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration. </param>
+        public RedirectOnAuthenticationFailureHandler(RedirectOnAuthenticationFailureHandlerConfigurationSection config)
             : base(config) { }
 
         #region IHttpHeaderInspectingAuthenticationFailureHandler Members
+        /// <summary>   Implements the authentication failure action, redirecting to a specified Uri. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="context">          The incoming request context. </param>
+        /// <param name="inspectorResults"> The failed inspector results. </param>
+        /// <returns>   Always returns a null IPrincipal. </returns>
         public override IPrincipal OnAuthenticationFailure(HttpContextBase context, 
             Dictionary<IHttpContextInspectingAuthenticator, InspectorAuthenticationResult> inspectorResults)
         {
