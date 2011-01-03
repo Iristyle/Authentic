0997fae5dd057cb2e8b9a3715bbe8db49ddc1dae
ebrown <devnull@localhost>
2011-01-03 16:22:58 -0500

    added docs

--- EPS.Web.Authentication/Abstractions/IHttpContextInspectingAuthenticationFailureHandlerFactory.cs
+++ EPS.Web.Authentication/Abstractions/IHttpContextInspectingAuthenticationFailureHandlerFactory.cs
@@ -3,15 +3,38 @@ using EPS.Web.Authentication.Configuration;
 
 namespace EPS.Web.Authentication.Abstractions
 {
+    /// <summary>   
+    /// Interface for the http context inspecting authentication failure handler factory.  Constructs a failure handler given configuration
+    /// information. 
+    /// </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public interface IHttpContextInspectingAuthenticationFailureHandlerFactory
     {
+        /// <summary>   
+        /// Constructs a <see cref="EPS.Web.Authentication.Abstractions.IHttpContextInspectingAuthenticationFailureHandler"/>
+        /// given configuration information. 
+        /// </summary>
+        /// <param name="config">   The configuration. </param>
+        /// <returns>   An instance of a failure handler. </returns>
         IHttpContextInspectingAuthenticationFailureHandler Construct(HttpContextInspectingAuthenticationFailureConfigurationSection config);
     }
 
+    /// <summary>   
+    /// Generic version of the <see cref="EPS.Web.Authentication.Abstractions.IHttpContextInspectingAuthenticationFailureHandlerFactory"/>
+    /// interface for the http context inspecting authentication failure handler factory.  Constructs a failure handler given specialized
+    /// configuration information. 
+    /// </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public interface IHttpContextInspectingAuthenticationFailureHandlerFactory<T> : 
         IHttpContextInspectingAuthenticationFailureHandlerFactory
         where T : HttpContextInspectingAuthenticationFailureConfigurationSection
     {
+        /// <summary>   
+        /// Constructs a generic IHttpContextInspectingAuthenticationFailureHandler, which allows passing along the specific type of
+        /// configuration information. 
+        /// </summary>
+        /// <param name="config">   The configuration. </param>
+        /// <returns>   An instance of a failure handler. </returns>
         IHttpContextInspectingAuthenticationFailureHandler<T> Construct(T config);
     }
 
