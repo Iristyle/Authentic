051f01f27cb16a045c287d3986758eaad5119235
ebrown <devnull@localhost>
2011-01-03 16:25:53 -0500

    added docs reworked constructor added legit FxCop suppressions

--- EPS.Web.Authentication/SimpleAuthenticationFailureHandler.cs
+++ EPS.Web.Authentication/SimpleAuthenticationFailureHandler.cs
@@ -1,29 +1,40 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Principal;
 using System.Web;
-using EPS.Web.Authentication.Configuration;
 using EPS.Web.Authentication.Abstractions;
+using EPS.Web.Authentication.Configuration;
 
 namespace EPS.Web.Authentication
 {
+    /// <summary>   A very simple authentication failure handler that allows for clients to simply hook a failure event. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class SimpleAuthenticationFailureHandler :
                HttpContextInspectingAuthenticationFailureHandlerBase<SimpleAuthenticationFailureHandlerConfigurationSection>
     {
-        internal SimpleAuthenticationFailureHandler(SimpleAuthenticationFailureHandlerConfigurationSection config)
+        /// <summary>   Constructor an instance of the failure handler given the configuration. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration (empty). </param>
+        public SimpleAuthenticationFailureHandler(SimpleAuthenticationFailureHandlerConfigurationSection config)
             : base(config) { }
 
-        /// <summary>
-        /// Implementors must set the IPrincipal here to something
-        /// </summary>
-        //TODO: 8-9-2010 -- prevent against multiple hooks here
+        /// <summary> The authentication failure handler -- implementors must set the IPrincipal here to something
+        /// </summary>   
+        [SuppressMessage("Microsoft.Usage", "CA2211:NonConstantFieldsShouldNotBeVisible", Justification = "Add/remove is thread-safe for events in .NET.")]
         public static EventHandler<SimpleAuthenticationFailureEventArgs> AuthenticationFailure;
+        //TODO: 8-9-2010 -- prevent against multiple hooks here
 
         #region IHttpHeaderInspectingAuthenticationFailureHandler Members
+        /// <summary>   Executes the authentication failure action. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="context">          The incoming HttpContextBase. </param>
+        /// <param name="inspectorResults"> The set of failed inspector results. </param>
+        /// <returns>   An IPrincipal instance as returned by the failure event handler. </returns>
         public override IPrincipal OnAuthenticationFailure(HttpContextBase context,
             Dictionary<IHttpContextInspectingAuthenticator, InspectorAuthenticationResult> inspectorResults)
         {
-            var eventArgs = new SimpleAuthenticationFailureEventArgs(config, context, inspectorResults);
+            var eventArgs = new SimpleAuthenticationFailureEventArgs(Configuration, context, inspectorResults);
 
             if (null != AuthenticationFailure)
                 AuthenticationFailure(this, eventArgs);
