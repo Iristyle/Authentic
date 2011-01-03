ecbc30eab4def16d07686d0095e69e0c43846840
ebrown <devnull@localhost>
2011-01-03 16:26:15 -0500

    added docs reworked field accessibility

--- EPS.Web.Authentication/Security/FixedProviderRolePrincipal.cs
+++ EPS.Web.Authentication/Security/FixedProviderRolePrincipal.cs
@@ -4,21 +4,36 @@ using System.Web.Security;
 
 namespace EPS.Web.Authentication.Security
 {
+    /// <summary>   
+    /// A very simle IPrincipal implementation that supports attaching an arbitrary IIdentity and RoleProvider to determine role membership.. 
+    /// </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class FixedProviderRolePrincipal : IPrincipal
     {
-        private RoleProvider roleProvider;
-        private IIdentity identity;
+        private readonly RoleProvider roleProvider;
+        private readonly IIdentity identity;
+        
+        /// <summary>   Constructor an instance of the principal. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="roleProvider"> The role provider. </param>
+        /// <param name="identity">     The identity. </param>
         public FixedProviderRolePrincipal(RoleProvider roleProvider, IIdentity identity)
         {
             this.roleProvider = roleProvider;
             this.identity = identity;
         }
 
+        /// <summary>   Determines whether the current principal belongs to the specified role. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="role"> The name of the role for which to check membership. </param>
+        /// <returns>   true if the current principal is a member of the specified role; otherwise, false. </returns>
         public bool IsInRole(string role)
         {
             return roleProvider.IsUserInRole(identity.Name, role);
         }
 
+        /// <summary>   Gets the identity of the current principal. </summary>
+        /// <value> The <see cref="T:System.Security.Principal.IIdentity" /> object associated with the current principal. </value>
         public IIdentity Identity
         {
             get { return this.identity; }
