a0ae913eaf3daaca753814c642746fce4edcfbf5
ebrown <devnull@localhost>
2011-01-03 16:24:44 -0500

    added docs

--- EPS.Web.Authentication/Basic/BasicAuthenticationInspectingAuthenticatorFactory.cs
+++ EPS.Web.Authentication/Basic/BasicAuthenticationInspectingAuthenticatorFactory.cs
@@ -1,13 +1,23 @@
 using System;
-using EPS.Web.Authentication.Basic.Configuration;
 using EPS.Web.Authentication.Abstractions;
+using EPS.Web.Authentication.Basic.Configuration;
 
 namespace EPS.Web.Authentication.Basic
 {
+    /// <summary>   An implementation of a simple Basic authentication inspecting authenticator factory. </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class BasicAuthenticationInspectingAuthenticatorFactory :
         HttpContextInspectingAuthenticatorFactoryBase<BasicAuthenticationHeaderInspectorConfigurationElement>
     {
         #region IHttpHeaderInspectingAuthenticatorFactory Members
+        /// <summary>   
+        /// Constructs a new instance of a <see cref="T:EPS.Web.Authentication.Basic.BasicAuthenticationInspectingAuthenticator"/>, returning a
+        /// <see cref="T:
+        /// EPS.Web.Authentication.Abstractions.IHttpContextInspectingAuthenticator{BasicAuthenticationHeaderInspectorConfigurationElement}"/> . 
+        /// </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration. </param>
+        /// <returns>   A new authenticator instance. </returns>
         public override IHttpContextInspectingAuthenticator<BasicAuthenticationHeaderInspectorConfigurationElement> Construct(BasicAuthenticationHeaderInspectorConfigurationElement config)
         {
             return new BasicAuthenticationInspectingAuthenticator(config);
