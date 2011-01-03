c14e94794c025591a229bf9651239827e2162f9c
ebrown <devnull@localhost>
2011-01-03 16:30:28 -0500

    added docs

--- EPS.Web.Authentication/Configuration/RedirectOnAuthenticationFailureHandlerConfigurationSection.cs
+++ EPS.Web.Authentication/Configuration/RedirectOnAuthenticationFailureHandlerConfigurationSection.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Configuration;
-using EPS.Web.Authentication.Configuration;
 
 namespace EPS.Web.Authentication.Configuration
 {
+    /// <summary>   
+    /// Redirect on authentication failure handler configuration section that defines the configurable behavior of
+    /// <see cref="T:EPS.Web.Authentication.RedirectOnAuthenticationFailureHandler"/>. 
+    /// </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
     public class RedirectOnAuthenticationFailureHandlerConfigurationSection : HttpContextInspectingAuthenticationFailureConfigurationSection
     {
+        /// <summary>   Gets or sets URI for the redirect on a failed request. </summary>
+        /// <value> The redirect uri. </value>
         [ConfigurationProperty("redirectUri", IsRequired = true)]
         public Uri RedirectUri
         {
