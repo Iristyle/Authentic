f55a2db4f2aaabcb928bdb437032decf1451f214
ebrown <devnull@localhost>
2011-01-05 14:15:59 -0500

    null checking per FxCop

--- EPS.Web.Authentication/Utility/IPrincipalExtensions.cs
+++ EPS.Web.Authentication/Utility/IPrincipalExtensions.cs
@@ -12,19 +12,25 @@ namespace EPS.Web.Authentication.Utility
     {
         /// <summary>   An IPrincipal extension method that querys if an 'IPrincipal' has an email address. </summary>
         /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
         /// <param name="principal">    The principal to act on. </param>
         /// <returns>   true if email address, false if not. </returns>
         public static bool HasEmailAddress(this IPrincipal principal)
         {
+            if (null == principal) { throw new ArgumentNullException("principal"); }
+
             return !string.IsNullOrEmpty(GetEmailAddress(principal));
         }
 
         /// <summary>   An IPrincipal extension method that gets an email address. </summary>
         /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
         /// <param name="principal">    The principal to act on. </param>
         /// <returns>   The email address. </returns>
         public static string GetEmailAddress(this IPrincipal principal)
         {
+            if (null == principal) { throw new ArgumentNullException("principal"); }
+
             /*
             if (principal is OnyxPrincipal)
                 return ((OnyxPrincipal)principal).Identity.UserProperties.Email;
