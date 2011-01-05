be70e1f07502ff82a81465c27202d075e114ccd0
ebrown <devnull@localhost>
2011-01-05 12:58:10 -0500

    added suppresions for namespaces

---
+++ EPS.Web.Tests.Unit/Runtime/Caching/ThreadSafeCacheManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EPS.Runtime.Caching;
+using FakeItEasy;
+using Xunit;
+
+namespace EPS.Runtime.Caching.Tests.Unit
+{
+    public interface IThreadSafeCacheManagerTest
+    {
+        void TestSuite();
+    }
+    public class ThreadSafeCacheManagerTest : IThreadSafeCacheManagerTest
+    {
+        [Fact]
+        public void TestSuite()
+        {
+            var cacheManager = ThreadSafeCacheManager<object>.Construct("myObjectCache", TimeSpan.MaxValue);            
+            throw new NotImplementedException();
+        }
+    }
+}
