354181fec8f484a1267dd682b9566594112107ea
ebrown <devnull@localhost>
2011-01-23 20:19:22 -0500

    CustomDictionary.xml has been added to all projects as a linked CodeAnalysisDictionary file -- so that abbreviations like EPS are ignored removed all suppressions for CA1709 as they pertain to 'EPS' as the custom dictionary now handles this

--- EPS.Web.Authentication/Properties/AssemblyInfo.cs
+++ EPS.Web.Authentication/Properties/AssemblyInfo.cs
@@ -18,14 +18,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3BFA6D41-DA95-448F-A0EB-67CC4CF67673")]
 
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication", Scope = "assembly", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication.Abstractions", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication.Basic", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication.Basic.Configuration", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication.Configuration", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication.Security", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Web.Authentication.Utility", Scope = "namespace", Justification = "EPS is a proper company name")]
-
 [assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Target = "EPS.Web.Authentication.Security", Scope = "namespace", Justification = "Most appropriate namespace structure")]
 [assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Target = "EPS.Web.Authentication.Utility", Scope = "namespace", Justification = "Most appropriate namespace structure")]
+
+//[assembly: SuppressMessage("Gendarme.Rules.Naming", "AvoidDeepNamespaceHierarchyRule", Target = "EPS.Web.Authentication", Scope = "assembly", Justification = "Configuration is a reasonable special name for a fifth level")]
