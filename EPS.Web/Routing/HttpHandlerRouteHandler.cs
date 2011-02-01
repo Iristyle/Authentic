a05a00c1967866b7751d9233803a9ab43e3df5f0
ebrown <devnull@localhost>
2011-02-01 13:45:22 -0500

    fixed docs

--- EPS.Web/Routing/HttpHandlerRouteHandler.cs
+++ EPS.Web/Routing/HttpHandlerRouteHandler.cs
@@ -5,7 +5,7 @@ using System.Web.Routing;
 namespace EPS.Web.Routing
 {
     /// <summary>   A custom IRouteHandler class that allows us to plug standard IHttpHandler instances into routing easier. </summary>
-    /// <remarks>   Shamelessly stolen from Haacked. <a href="http://haacked.com/archive/2009/11/04/routehandler-for-http-handlers.aspx"> </remarks>
+    /// <remarks>   Shamelessly stolen from Haacked. <a href="http://haacked.com/archive/2009/11/04/routehandler-for-http-handlers.aspx" /> </remarks>
     public class HttpHandlerRouteHandler<THandler> 
         : IRouteHandler where THandler : IHttpHandler, new()
     {
