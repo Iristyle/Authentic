95fdd994b14a4ae9da24689d16a7734d322f7dfb
ebrown <devnull@localhost>
2011-02-07 17:22:24 -0500

    refactored test code to avoid duplication now using GenericEqualityComparer<T> to do quick and dirty object comparisons

--- EPS.Web.Tests.Unit/HttpHandlerExtensionsTest.cs
+++ EPS.Web.Tests.Unit/HttpHandlerExtensionsTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
 using System.Web.Routing;
+using EPS.Utility;
 using EPS.Web.Routing;
 using Xunit;
 
@@ -28,31 +30,69 @@ namespace EPS.Web.Tests.Unit
             var insertedRoute = (Route)routes[0];
             return insertedRoute;
         }
-        [Fact]
 
-        public void MapHttpHandler_AddsNewRouteToHandler()
+        private GenericEqualityComparer<Route> routeComparer = new GenericEqualityComparer<Route>((route1, route2) =>
+                {
+                    return route1.Url == route2.Url && 
+                        route1.Defaults.SequenceEqual(route2.Defaults) && 
+                        route1.Constraints.SequenceEqual(route2.Constraints);
+                });
+
+        [Fact]
+        public void MapHttpHandler_Throws_OnEmptyUrl()
         {
-            var insertedRoute = GetRouteInsertedToCollection(routes => routes.MapHttpHandler<HttpHandlerTest>("url"));
-            Assert.True(insertedRoute.RouteHandler.GetType() == typeof(HttpHandlerRouteHandler<HttpHandlerTest>) 
-                && insertedRoute.Url == "url");
+            var routes = new RouteCollection();
+            Assert.Throws<ArgumentException>(() => routes.MapHttpHandler<HttpHandlerTest>(string.Empty));
         }
 
         [Fact]
-        public void MapHttpHandler_AddsNewRouteToHandlerWithNullDefaultsAndConstraints()
+        public void MapHttpHandler_NewRouteHandler_IsCorrectType()
+        {
+            var insertedRoute = GetRouteInsertedToCollection(routes => routes.MapHttpHandler<HttpHandlerTest>("route"));
+            Assert.IsType<HttpHandlerRouteHandler<HttpHandlerTest>>(insertedRoute.RouteHandler);
+        }
+
+        [Fact]
+        public void MapHttpHandler_NewRouteHandler_IsCorrectType_Overload()
         {
             var insertedRoute = GetRouteInsertedToCollection(routes => routes.MapHttpHandler<HttpHandlerTest>("name", "route-url", null, null));
-            Assert.True(insertedRoute.RouteHandler.GetType() == typeof(HttpHandlerRouteHandler<HttpHandlerTest>)
-                && insertedRoute.Url == "route-url");
+            Assert.IsType<HttpHandlerRouteHandler<HttpHandlerTest>>(insertedRoute.RouteHandler);
         }
 
         [Fact]
-        public void MapHttpHandler_AddsNewRouteToHandlerWithCorrectDefaultsAndConstraints()
+        public void MapHttpHandler_NewRouteHandler_UrlMatches()
+        {
+            var url = "url";
+            var insertedRoute = GetRouteInsertedToCollection(routes => routes.MapHttpHandler<HttpHandlerTest>(url));
+            Assert.True(insertedRoute.Url == url);
+        }
+
+        [Fact]
+        public void MapHttpHandler_NewRouteHandler_DefaultsAndConstraintsNotRequired()
+        {
+            var routes = new RouteCollection();
+            routes.MapHttpHandler<HttpHandlerTest>("name", "route", null, null);
+            Assert.True(routes.Count == 1);
+        }
+
+        [Fact]
+        public void MapHttpHandler_NewRouteHandler_UrlMatches_Overload()
+        {
+            var routeUrl = "route-url";
+            var insertedRoute = GetRouteInsertedToCollection(routes => routes.MapHttpHandler<HttpHandlerTest>("name", routeUrl, null, null));
+            Assert.True(insertedRoute.Url == routeUrl);
+        }
+
+        [Fact]
+        public void MapHttpHandler_NewRouteHandler_DefaultsAndConstraintsMatch()
         {
             var routes = new RouteCollection();
             routes.MapHttpHandler<HttpHandlerTest>("name", "url/{id}", new { id = 12 }, new { id = @"\d+" });
             var insertedRoute = (Route)routes[0];
-            Assert.True(insertedRoute.RouteHandler.GetType() == typeof(HttpHandlerRouteHandler<HttpHandlerTest>)
-                && insertedRoute.Url == "url/{id}" && (int)insertedRoute.Defaults["id"] == 12 && (string)insertedRoute.Constraints["id"] == @"\d+" );
+
+            var matchingRoute = new Route("url/{id}", new RouteValueDictionary(new { id = 12 }), new RouteValueDictionary(new { id = @"\d+" }), new HttpHandlerRouteHandler<HttpHandlerTest>());
+
+            Assert.Equal(insertedRoute, matchingRoute, routeComparer);
         }
 
         [Fact]
