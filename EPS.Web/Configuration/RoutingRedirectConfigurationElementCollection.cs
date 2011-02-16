dd399ce397e46f9d1f34533e9f208f21cb342ab4
ebrown <devnull@localhost>
2011-02-16 15:25:54 -0500

    removed GetUrlMap method because we're not using it directly anymore parent class exposes an IDictionary<string, RoutingRedirectConfigurationElement> instead of this collection

--- EPS.Web/Configuration/RoutingRedirectConfigurationElementCollection.cs
+++ EPS.Web/Configuration/RoutingRedirectConfigurationElementCollection.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using EPS.Configuration;
 
 namespace EPS.Web.Configuration
@@ -9,7 +6,7 @@ namespace EPS.Web.Configuration
     /// <summary>   Collection of RoutingRedirectConfigurationElements read from config. </summary>
     /// <remarks>   ebrown, 11/10/2010. </remarks>
     public class RoutingRedirectConfigurationElementCollection : 
-        ConfigurationElementCollection<string, RoutingRedirectConfigurationElement>
+        ConfigurationElementCollection<string, RoutingRedirectConfigurationElement>        
     {
         /// <summary>   Uses SourceUrl as a key. </summary>
         /// <remarks>   ebrown, 11/10/2010. </remarks>
@@ -21,15 +18,6 @@ namespace EPS.Web.Configuration
             if (null == element) { throw new ArgumentNullException("element"); }
 
             return element.SourceUrl;
-        }
-
-        /// <summary>   Gets a mapping of source url to target url as defined in config. </summary>
-        /// <remarks>   ebrown, 11/10/2010. </remarks>
-        /// <returns>   The url map. </returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Not a candidate for a property since it performs a Linq query / Dictionary construction")]
-        public Dictionary<string, string> GetUrlMap()
-        {
-            return this.OfType<RoutingRedirectConfigurationElement>().ToDictionary(r => r.SourceUrl, r => r.TargetUrl);
         }
     }
 }
