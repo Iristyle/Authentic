34984c01a529685b2a4c5b1c612dec441708776a
ebrown <devnull@localhost>
2011-02-16 15:37:06 -0500

    initial take on the LoadedStream class returned by IFileHttpHandlerStreamLoader initial take on status values set in the LoadedStream class, as represented by StreamLoadStatus

---
+++ EPS.Web/Handlers/StreamLoadStatus.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace EPS.Web.Handlers
+{
+    /// <summary>   
+    /// These specific values represent the status of a given FileStreamDetails as requested through the IFileHttpHandlerStreamLoader. 
+    /// </summary>
+    /// <remarks>   ebrown, 2/14/2011. </remarks>
+    public enum StreamLoadStatus
+    {
+        /// <summary> The FileStreamDetails were successfully loaded.  </summary>
+        Success,
+        /// <summary> The given request was invalid.  </summary>
+        InvalidRequest,
+        /// <summary> The request was denied based on the requesting user / security.  </summary>
+        UnauthorizedAccess,
+        /// <summary> No file metadata was found for the given contextual request information.  </summary>
+        FileMetadataNotFound,
+        /// <summary> File metadata was found, but a physical file could not be located.  </summary>
+        FileNotFound,
+        /// <summary> File metadata was found, and the file resides at an alternate Uri.  </summary>
+        FileStoredInCloud,
+        /// <summary> File metadata was found, but the actual FileStream found did not match the MD5 metadata stored.  </summary>
+        MD5Failed,
+        /// <summary> File metadata was found, but the actual FileStream size did not match the file size metadata stored.  </summary>
+        FileSizeMismatched,
+        /// <summary> An unexpected error occurred.  </summary>
+        UnexpectedError,
+    }
+}
