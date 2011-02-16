1bc4f79b1e83f37326c996dc565c01398c75ab0e
ebrown <devnull@localhost>
2011-02-16 15:23:26 -0500

    renamed IRoutingConfigurationSection -> IRoutingConfiguration so that no attachment to ConfigurationSection is implied made class more usable in tests by making PermanentRedirects a IDictionary<string, RoutingRedirectConfigurationElement> instead of RoutingRedirectConfigurationElementCollection

--- EPS.Web/Configuration/IRoutingConfiguration.cs
+++ EPS.Web/Configuration/IRoutingConfiguration.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 namespace EPS.Web.Configuration
 {
     /// <summary>   The interface for the routing configuration section. </summary>
     /// <remarks>   ebrown, 2/9/2011. </remarks>
-    public interface IRoutingConfigurationSection
+    public interface IRoutingConfiguration
     {
         /// <summary>   Gets the permanent redirects of source to target URL. </summary>
         /// <value> The permanent redirects. </value>
-        RoutingRedirectConfigurationElementCollection PermanentRedirects { get; }
+        IDictionary<string, RoutingRedirectConfigurationElement> PermanentRedirects { get; }
         
         /// <summary>   Gets a value indicating whether permanent redirects are enabled. </summary>
         /// <value> true if enabled, false if not. </value>
