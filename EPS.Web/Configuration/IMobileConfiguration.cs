f0f7f3c1d1f1612b0f278be52f272471464d5e89
ebrown <devnull@localhost>
2011-02-16 15:22:23 -0500

    renamed IMobileConfigurationSection -> IMobileConfiguration so that no attachment to ConfigurationSection is implied

--- EPS.Web/Configuration/IMobileConfiguration.cs
+++ EPS.Web/Configuration/IMobileConfiguration.cs
@@ -4,7 +4,7 @@ namespace EPS.Web.Configuration
 {
     /// <summary>   The interface for a configuration section that allows us to define settings as they apply to mobile devices. </summary>
     /// <remarks>   ebrown, 2/10/2011. </remarks>
-    public interface IMobileConfigurationSection
+    public interface IMobileConfiguration
     {
         /// <summary>   Gets the name of the cookie that can be used to override mobile or standard view. </summary>
         /// <value> The cookie name. </value>
