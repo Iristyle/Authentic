226eb8c80b6478295438d0bc98cd4e62ba00fbdf
ebrown <devnull@localhost>
2011-02-16 11:42:18 -0500

    HttpMethodNames has been renamed HttpMethodName instead of using a bunch of static strings in a static class, it has become an enum with EnumValue attributes for string values tied to the enums wrote some simple tests verifying the expected values match the constants

---
+++ EPS.Web.Tests.Unit/HttpMethodNamesTest.cs
@@ -0,0 +1,51 @@
+using System;
+using EPS.Text;
+using Xunit;
+
+namespace EPS.Web.Tests.Unit
+{
+    public class HttpMethodNamesTest
+    {
+        [Fact]
+        public void Get_HasProperEnumValue()
+        {
+            Assert.Equal("GET", HttpMethodNames.Get.ToEnumValueString());
+        }
+
+        [Fact]
+        public void Header_HasProperEnumValue()
+        {
+            Assert.Equal("HEAD", HttpMethodNames.Header.ToEnumValueString());
+        }
+
+        [Fact]
+        public void Put_HasProperEnumValue()
+        {
+            Assert.Equal("PUT", HttpMethodNames.Put.ToEnumValueString());
+        }
+
+        [Fact]
+        public void Post_HasProperEnumValue()
+        {
+            Assert.Equal("POST", HttpMethodNames.Post.ToEnumValueString());
+        }
+
+        [Fact]
+        public void Delete_HasProperEnumValue()
+        {
+            Assert.Equal("DELETE", HttpMethodNames.Delete.ToEnumValueString());
+        }
+
+        [Fact]
+        public void Trace_HasProperEnumValue()
+        {
+            Assert.Equal("TRACE", HttpMethodNames.Trace.ToEnumValueString());
+        }
+
+        [Fact]
+        public void Options_HasProperEnumValue()
+        {
+            Assert.Equal("OPTIONS", HttpMethodNames.Options.ToEnumValueString());
+        }
+    }
+}
--- EPS.Web/HttpMethodNames.cs
+++ EPS.Web/HttpMethodNames.cs
@@ -1,30 +1,39 @@
 using System;
+using EPS.Text;
 
 namespace EPS.Web
 {
     /// <summary>   Http method definition names (i.e verbs) as defined in RFC2616 <a href="http://tools.ietf.org/html/rfc2616#section-9" />. </summary>
     /// <remarks>   Used when manually baking up HTTP request / responses. </remarks>
-    public static class HttpMethodNames
+    [Flags]
+    public enum HttpMethodNames
     {
         /// <summary> The GET method means retrieve whatever information (in the form of an entity) is identified by the Request-URI </summary>
-        public static readonly string Get = "GET";
+        [EnumValue("GET")]
+        Get = 1,
         
         /// <summary> The HEAD method is identical to GET except that the server MUST NOT return a message-body in the response. </summary>
-        public static readonly string Header = "HEAD";
+        [EnumValue("HEAD")]
+        Header = (1 << 1),
         
         /// <summary> The PUT method requests that the enclosed entity be stored under the supplied Request-URI. </summary>
-        public static readonly string Put = "PUT";
+        [EnumValue("PUT")]
+        Put = (1 << 2),
         
         /// <summary> The POST method is used to request that the origin server accept the entity enclosed in the request as a new subordinate of the resource identified by the Request-URI in the Request-Line. </summary>
-        public static readonly string Post = "POST";
+        [EnumValue("POST")]
+        Post = (1 << 3),
         
         /// <summary> The DELETE method requests that the origin server delete the resource identified by the Request-URI. </summary>
-        public static readonly string Delete = "DELETE";
+        [EnumValue("DELETE")]
+        Delete = (1 << 4),
         
         /// <summary> The TRACE method is used to invoke a remote, application-layer loop-back of the request message. </summary>
-        public static readonly string Trace = "TRACE";
+        [EnumValue("TRACE")]
+        Trace = (1 << 5),
         
         /// <summary> The OPTIONS method represents a request for information about the communication options available on the request response chain identified by the Request-URI </summary>
-        public static readonly string Options = "OPTIONS";
+        [EnumValue("OPTIONS")]
+        Options = (1 << 6)
     }
 }
