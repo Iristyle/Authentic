b2980071cc6c906ce1d444654aed23cba8619fe6
ebrown <devnull@localhost>
2011-03-28 18:32:34 -0400

    inital commit of opaque generator

---
+++ EPS.Web.Authentication/Digest/Opaque.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace EPS.Web.Authentication.Digest
+{
+    /// <summary>   
+    /// This is an opaque value that is passed from server to client and back again. Implementing something random that is unique to a client
+    /// is on a load balanced server farm is quite a difficult task, so we punt on that and use something simple.
+    /// </summary>
+    /// <remarks>   ebrown, 3/28/2011. </remarks>
+    public static class Opaque
+    {
+        /// <summary> A user-replaceable opaque value used during digest authentication. </summary>
+        public static Func<string> Current = () => "1234123412345678";
+    }
+}
