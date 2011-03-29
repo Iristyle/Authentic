5b331d4934f10c7869f24f98c375ef0354bb2170
ebrown <devnull@localhost>
2011-03-29 17:01:45 -0400

    added tests for MembershipProviderLocator also added utility class for adding to membership providers at will

---
+++ EPS.Web.Authentication.Tests.Unit/Utility/MembershipProviderLocatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Security;
+using FakeItEasy;
+using Xunit;
+
+namespace EPS.Web.Authentication.Utility.Tests.Unit
+{
+    public class MembershipProviderLocatorTest : IDisposable
+    {
+        private readonly MembershipProvider fakeMembershipProvider = A.Fake<MembershipProvider>();
+
+        public MembershipProviderLocatorTest()
+        {
+            A.CallTo(() => fakeMembershipProvider.Name).Returns("Fake");
+            Membership.Providers.AddMembershipProvider(fakeMembershipProvider);            
+        }
+
+        public void Dispose()
+        {
+            Membership.Providers.RemoveMembershipProvider("Fake");
+        }
+
+        [Fact]
+        public void GetProvider_FindsRegisteredProviderByName()
+        {
+            Assert.Same(fakeMembershipProvider, MembershipProviderLocator.GetProvider("Fake"));
+        }
+
+        [Fact]
+        public void GetProvider_ThrowsOnUnregisteredProviderName()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => MembershipProviderLocator.GetProvider("MissingProvider"));
+        }
+
+        [Fact]
+        public void GetProvider_DefaultStringMatchesDefaultRegisteredProvider()
+        {
+            Assert.Same(Membership.Provider, MembershipProviderLocator.GetProvider("default"));
+        }
+
+        [Fact]
+        public void GetProvider_ReturnsNullOnNullProviderName()
+        {
+            Assert.Null(MembershipProviderLocator.GetProvider(null));
+        }
+
+        [Fact]
+        public void GetProvider_ReturnsNullOnEmptyProviderName()
+        {
+            Assert.Null(MembershipProviderLocator.GetProvider(string.Empty));
+        }
+    }
+}
---
+++ EPS.Web.Authentication.Tests.Unit/Utility/ProviderCollectionExtensions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Configuration.Provider;
+using System.Reflection;
+using System.Web.Security;
+
+namespace EPS.Web.Authentication.Utility.Tests.Unit
+{
+    //some nice hacks ;0
+    //http://owlsayswoot.therandomist.com/2010/07/28/mocking-membership-provider/
+    public static class ProviderCollectionExtensions
+    {
+        //hacks around the fact that we can't call ProviderCollection.Add
+        public static void AddMembershipProvider(this ProviderCollection providers, MembershipProvider provider)
+        {
+            GetMembershipHashtable().Add(provider.Name, provider);
+        }
+
+        public static void AddMembershipProvider(this ProviderCollection providers, string providerName, MembershipProvider provider)
+        {
+            GetMembershipHashtable().Add(providerName, provider);
+        }
+
+        public static void RemoveMembershipProvider(this ProviderCollection providers, string providerName)
+        {
+            GetMembershipHashtable().Remove(providerName);
+        }
+
+        static Hashtable GetMembershipHashtable()
+        {
+            var hashtableField = typeof(ProviderCollection).GetField("_Hashtable", BindingFlags.Instance | BindingFlags.NonPublic);
+            return hashtableField.GetValue(Membership.Providers) as Hashtable;
+        }
+    }
+}
