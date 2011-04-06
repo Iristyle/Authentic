c5b543dbaa5937639b44e8ba5ab94f50fc044331
ebrown <devnull@localhost>
2011-04-06 10:34:22 -0400

    the real meat of the authentication process for digest auth provided a first-cut set of unit tests demonstrating how it works

---
+++ EPS.Web.Authentication.Tests.Unit/Digest/AuthenticationInspectingAuthenticatorTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using EPS.Web.Abstractions;
+using EPS.Web.Authentication.Digest.Configuration;
+using FakeItEasy;
+using Xunit;
+
+namespace EPS.Web.Authentication.Digest.Tests.Unit
+{
+    public class AuthenticationInspectingAuthenticatorTest : IDisposable
+    {
+        [Fake] readonly MembershipProvider fakeMembershipProvider;
+        [Fake] MembershipUser fakeUser;
+        [Fake] IAuthenticationHeaderInspectorConfigurationElement configuration;
+        [Fake] HttpContextBase context;
+        [Fake] IPrincipalBuilder principalBuilder;
+
+        public AuthenticationInspectingAuthenticatorTest()
+        {
+            Fake.InitializeFixture(this);
+
+            A.CallTo(() => fakeUser.GetPassword()).Returns("Circle Of Life");
+            A.CallTo(() => fakeMembershipProvider.Name).Returns("Fake");
+            A.CallTo(() => fakeMembershipProvider.GetUser("Mufasa", A<Boolean>.Ignored)).Returns(fakeUser);
+            Membership.Providers.AddMembershipProvider(fakeMembershipProvider);
+
+            A.CallTo(() => configuration.Factory).Returns(typeof(AuthenticationInspectingAuthenticatorFactory).FullName);
+            A.CallTo(() => configuration.ProviderName).Returns(fakeMembershipProvider.Name);
+            A.CallTo(() => configuration.GetPrincipalBuilder()).Returns(principalBuilder);
+            A.CallTo(() => principalBuilder.ConstructPrincipal(A<HttpContextBase>.Ignored, A<string>.Ignored, A<string>.Ignored))
+                .ReturnsLazily(call => new GenericPrincipal(new GenericIdentity(call.GetArgument<string>("userName")), new[] { "roles" }));
+        }
+
+        public void Dispose()
+        {
+            Membership.Providers.RemoveMembershipProvider("Fake");
+        }
+
+        [Fact]
+        public void Authenticate_ThrowsOnNullContext()
+        {
+            var inspector = new AuthenticationInspectingAuthenticator(configuration);
+            Assert.Throws<ArgumentNullException>(() => inspector.Authenticate(null));
+        }
+
+        [Fact]
+        public void Authenticate_TrueOnValidMembership()
+        {
+            A.CallTo(() => configuration.Realm).Returns("testrealm@host.com");
+
+            var headers = new NameValueCollection() { { "Authorization", 
+@"Digest username=""Mufasa"",realm=""testrealm@host.com"",
+                     nonce=""dcd98b7102dd2f0e8b11d0f600bfb0c093"",
+                     uri=""/dir/index.html"",qop=auth,nc=00000001,cnonce=""0a4f113b"",
+                     response=""6629fae49393a05397450978507c4ef1"",
+                     opaque=""5ccc069c403ebaf9f0171e9517f40e41""" } };
+
+            A.CallTo(() => context.Request.Headers).Returns(headers);
+            A.CallTo(() => context.Request.HttpMethod).Returns("GET");
+            Opaque.Current = () => "5ccc069c403ebaf9f0171e9517f40e41";
+            NonceCache.Current = () => "dcd98b7102dd2f0e8b11d0f600bfb0c093";
+
+            var inspector = new AuthenticationInspectingAuthenticator(configuration);
+            var result = inspector.Authenticate(context);
+            //TODO: validate that we have a GenericPrincipal / GenericIdentity
+            Assert.True(result.Success && result.Principal.Identity.Name == "Mufasa");
+        }
+
+        [Fact]
+        public void Authenticate_FalseOnMismatchedRealm()
+        {
+            A.CallTo(() => configuration.Realm).Returns("testrealm@testhost.com");
+
+            var headers = new NameValueCollection() { { "Authorization", 
+@"Digest username=""Mufasa"",realm=""testrealm@host.com"",
+                     nonce=""dcd98b7102dd2f0e8b11d0f600bfb0c093"",
+                     uri=""/dir/index.html"",qop=auth,nc=00000001,cnonce=""0a4f113b"",
+                     response=""6629fae49393a05397450978507c4ef1"",
+                     opaque=""5ccc069c403ebaf9f0171e9517f40e41""" } };
+
+            A.CallTo(() => context.Request.Headers).Returns(headers);
+            A.CallTo(() => context.Request.HttpMethod).Returns("GET");
+            Opaque.Current = () => "5ccc069c403ebaf9f0171e9517f40e41";
+            NonceCache.Current = () => "dcd98b7102dd2f0e8b11d0f600bfb0c093";
+
+            var inspector = new AuthenticationInspectingAuthenticator(configuration);
+            var result = inspector.Authenticate(context);
+            //TODO: validate that we have a GenericPrincipal / GenericIdentity
+            Assert.False(result.Success);
+        }
+
+        [Fact(Skip = "Write some more examples of auth failures")]
+        public void Authenticate_FalseOnAuthenticationFailures()
+        {
+        }
+    }
+}
---
+++ EPS.Web.Authentication/Digest/AuthenticationInspectingAuthenticator.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Globalization;
+using System.Security.Principal;
+using System.Web;
+using EPS.Text;
+using EPS.Web.Abstractions;
+using EPS.Web.Authentication.Abstractions;
+using EPS.Web.Authentication.Digest.Configuration;
+using EPS.Web.Authentication.Security;
+using EPS.Web.Authentication.Utility;
+using EPS.Web.Management;
+
+namespace EPS.Web.Authentication.Digest
+{
+    /// <summary>   
+    /// Digest authentication inspecting authenticator.  Inspects HTTP 'Authorization' headers and generates IPrincipals based on the configured
+    /// MembershipProvider. 
+    /// </summary>
+    /// <remarks>   ebrown, 1/3/2011. </remarks>
+    public class AuthenticationInspectingAuthenticator : 
+        HttpContextInspectingAuthenticatorBase<IAuthenticationHeaderInspectorConfigurationElement>
+    {
+        /// <summary>   Initializes a new instance of the AuthenticationInspectingAuthenticator class. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <param name="config">   The configuration. </param>
+        public AuthenticationInspectingAuthenticator(IAuthenticationHeaderInspectorConfigurationElement config) 
+            : base(config) {}
+        
+        /// <summary>   Authenticates a HttpContextBase given a specified MembershipProvider. </summary>
+        /// <remarks>   ebrown, 1/3/2011. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <exception cref="Exception">                Thrown when an unexpected exception occurs. </exception>
+        /// <param name="context">  The context. </param>
+        /// <returns>   A success or failure if the MembershipProvider validated the credentials found in the header. </returns>
+        public override InspectorAuthenticationResult Authenticate(HttpContextBase context)
+        {            
+            try
+            {
+                if (null == context) { throw new ArgumentNullException("context"); }
+
+                string authHeader = context.Request.Headers["Authorization"];
+            
+                Log.InfoFormat(CultureInfo.InvariantCulture, "Authorization header [{0}] received", authHeader.IfMissing("**Missing**"));
+
+                ////attempt to extract credentials from header
+                DigestHeader digestHeader;
+                if (!HttpDigestAuthHeaderParser.TryExtractDigestHeader(context.Request.HttpMethod.ToEnumFromEnumValue<HttpMethodNames>(), authHeader, out digestHeader))
+                {
+                    //don't log an event since there was nothing to succeed / fail
+                    return new InspectorAuthenticationResult(false, null, "No digest credentials found in HTTP header");
+                }
+
+                var membershipProvider = MembershipProviderLocator.GetProvider(Configuration.ProviderName);
+                if (null == membershipProvider)
+                {
+                    throw new ArgumentException("MembershipProvider specified in configuration cannot be found, but is required to lookup user password for comparison");
+                }
+
+                string userPassword = membershipProvider.GetUser(digestHeader.UserName, false).GetPassword();
+
+                if (digestHeader.MatchesCredentials(Configuration.Realm, NonceCache.Current(), Opaque.Current(), userPassword))
+                {
+                    if (null != membershipProvider)
+                    {
+                        new AuthenticationSuccessEvent(this, digestHeader.UserName).Raise();
+                    }
+
+                    IPrincipal principal = GetPrincipal(context, digestHeader.UserName, userPassword);
+                    IIdentity identity = null != principal ? principal.Identity : null;
+                    if (null != identity && identity.IsAuthenticated)
+                    {
+                        new AuthenticationSuccessEvent(this, identity.Name).Raise();
+                        return new InspectorAuthenticationResult(true, principal, string.Empty);
+                    }
+                }
+
+                new AuthenticationFailureEvent(this, digestHeader.UserName).Raise();
+                return new InspectorAuthenticationResult(false, null, string.Empty);
+            }
+            catch (Exception)
+            {
+                new AuthenticationFailureEvent(this, string.Empty).Raise();
+                throw;
+            }
+        }
+
+        private IPrincipal GetPrincipal(HttpContextBase context, string username, string password)
+        {
+            //if configuration specifies a plug-in principal builder, then use what's specified
+            //this allows us to accept digest auth credentials, but return custom principal objects
+            //i.e. digest username /password -> MyPrincipal!
+            IPrincipalBuilder principalBuilder = Configuration.GetPrincipalBuilder();
+            if (null != principalBuilder)
+                return principalBuilder.ConstructPrincipal(context, username, password);
+
+            //otherwise, use our generic identities / principals create principal and set Context.User
+            GenericIdentity id = new GenericIdentity(username, "CustomDigest");
+            if (!string.IsNullOrEmpty(Configuration.RoleProviderName))
+                return new FixedProviderRolePrincipal(RoleProviderHelper.GetProviderByName(Configuration.RoleProviderName), id);
+
+            return new GenericPrincipal(id, null);
+        }
+    }
+}
