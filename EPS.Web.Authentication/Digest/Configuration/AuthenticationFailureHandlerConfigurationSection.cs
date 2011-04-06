81a3b988ce44acbea2051b85934c8d678ba3ade5
ebrown <devnull@localhost>
2011-04-06 18:46:55 -0400

    full-blown config sections implementing config interfaces

---
+++ EPS.Web.Authentication/Digest/Configuration/AuthenticationFailureHandlerConfigurationSection.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Configuration;
+using EPS.Web.Authentication.Configuration;
+
+namespace EPS.Web.Authentication.Digest.Configuration
+{
+    /// <summary>   Digest authentication failure handler configuration section. </summary>
+    /// <remarks>   ebrown, 12/30/2010. </remarks>
+    public class AuthenticationFailureHandlerConfigurationSection : 
+        HttpContextInspectingAuthenticationFailureConfigurationSection, 
+        IAuthenticationFailureHandlerConfigurationSection
+    {
+        /// <summary>   Gets or sets the realm of the cookie on an outgoing cookie request. </summary>
+        /// <value> The realm. </value>
+        [ConfigurationProperty("realm", DefaultValue = "localhost")]
+        [StringValidator(MinLength = 1)]
+        public string Realm
+        {
+            get { return (string)this["realm"]; }
+            set { base["realm"] = value; }
+        }
+
+        /// <summary>   Gets or sets the Private Key value used when generating nonce values.  Minimum length of 8 characters. </summary>
+        /// <value> The private key. </value>
+        [ConfigurationProperty("privateKey", IsRequired = true, DefaultValue = "privateKey")]
+        [StringValidator(MinLength = 8)]
+        public string PrivateKey
+        {
+            get { return (string)this["privateKey"]; }
+            set { base["privateKey"] = value; }
+        }
+
+        /// <summary>   Gets or sets the timespan that a nonce is valid. </summary>
+        /// <value> The duration that the nonce is valid for. </value>
+        [ConfigurationProperty("nonceValidDuration", IsRequired = true, DefaultValue = "00:05:00")]
+        [TimeSpanValidator(MinValueString = "00:00:20", MaxValueString = "00:60:00")]
+        public TimeSpan NonceValidDuration
+        {
+            get { return (TimeSpan)this["nonceValidDuration"]; }
+            set { base["nonceValidDuration"] = value; }
+        }
+    }
+}
