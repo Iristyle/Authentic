301e15aec0bbfc20e1d3fcbe7e1c66feee591719
ebrown <devnull@localhost>
2011-04-20 10:55:29 -0400

    reformatted with tabs

--- EPS.Web.Tests.Unit/MimeTypesTest.cs
+++ EPS.Web.Tests.Unit/MimeTypesTest.cs
@@ -3,13 +3,13 @@ using Xunit;
 
 namespace EPS.Web.Tests.Unit
 {
-    public class MimeTypesTest
-    {
-        [Fact]
-        public void GetMimeTypeForFileExtension_ReturnsExpectedMimeTypeForHtml()
-        {
-            Assert.Same("text/HTML", MimeTypes.GetMimeTypeForFileExtension(".htm"));
-        }
+	public class MimeTypesTest
+	{
+		[Fact]
+		public void GetMimeTypeForFileExtension_ReturnsExpectedMimeTypeForHtml()
+		{
+			Assert.Same("text/HTML", MimeTypes.GetMimeTypeForFileExtension(".htm"));
+		}
 
 		[Fact]
 		public void GetMimeTypeForFileExtension_ReturnsExpectedMimeTypeForJpg()
@@ -17,40 +17,40 @@ namespace EPS.Web.Tests.Unit
 			Assert.Same("image/jpeg", MimeTypes.GetMimeTypeForFileExtension(".jpg"));
 		}
 
-        [Fact]
-        public void GetMimeTypeForFileExtension_ReturnsDefaultOfApplicationOctetStreamForUnregisteredMimeType()
-        {
-            Assert.Same("application/octet-stream", MimeTypes.GetMimeTypeForFileExtension(".poop"));
-        }
+		[Fact]
+		public void GetMimeTypeForFileExtension_ReturnsDefaultOfApplicationOctetStreamForUnregisteredMimeType()
+		{
+			Assert.Same("application/octet-stream", MimeTypes.GetMimeTypeForFileExtension(".poop"));
+		}
 
-        [Fact]
-        public void GetMimeTypeForFileExtension_ReturnsUserSuppliedForUnregisteredMimeType()
-        {
-            string userMimeType = "poopy";
-            Assert.Same(userMimeType, MimeTypes.GetMimeTypeForFileExtension(".poop", userMimeType));
-        }
+		[Fact]
+		public void GetMimeTypeForFileExtension_ReturnsUserSuppliedForUnregisteredMimeType()
+		{
+			string userMimeType = "poopy";
+			Assert.Same(userMimeType, MimeTypes.GetMimeTypeForFileExtension(".poop", userMimeType));
+		}
 
-        [Fact]
-        public void GetMimeTypeForFileExtension_ThrowsOnNullExtension()
-        {
-            Assert.Throws<ArgumentNullException>(() => MimeTypes.GetMimeTypeForFileExtension(null));
-        }
-        [Fact]
-        public void GetMimeTypeForFileExtension_ThrowsOnWhiteSpaceExtension()
-        {
-            Assert.Throws<ArgumentException>(() => MimeTypes.GetMimeTypeForFileExtension(string.Empty));
-        }
+		[Fact]
+		public void GetMimeTypeForFileExtension_ThrowsOnNullExtension()
+		{
+			Assert.Throws<ArgumentNullException>(() => MimeTypes.GetMimeTypeForFileExtension(null));
+		}
+		[Fact]
+		public void GetMimeTypeForFileExtension_ThrowsOnWhiteSpaceExtension()
+		{
+			Assert.Throws<ArgumentException>(() => MimeTypes.GetMimeTypeForFileExtension(string.Empty));
+		}
 
-        [Fact]
-        public void GetMimeTypeForFileExtension_ThrowsOnNullDefault()
-        {
-            Assert.Throws<ArgumentNullException>(() => MimeTypes.GetMimeTypeForFileExtension(".htm", null));
-        }
+		[Fact]
+		public void GetMimeTypeForFileExtension_ThrowsOnNullDefault()
+		{
+			Assert.Throws<ArgumentNullException>(() => MimeTypes.GetMimeTypeForFileExtension(".htm", null));
+		}
 
-        [Fact]
-        public void GetMimeTypeForFileExtension_ThrowsOnWhiteSpaceDefault()
-        {
-            Assert.Throws<ArgumentException>(() => MimeTypes.GetMimeTypeForFileExtension(".htm", string.Empty));
-        }
-    }
+		[Fact]
+		public void GetMimeTypeForFileExtension_ThrowsOnWhiteSpaceDefault()
+		{
+			Assert.Throws<ArgumentException>(() => MimeTypes.GetMimeTypeForFileExtension(".htm", string.Empty));
+		}
+	}
 }
