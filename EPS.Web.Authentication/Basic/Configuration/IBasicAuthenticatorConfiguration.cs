7bb2296e65cf00566e26ac06c85f36ff663dc7ed
ebrown <devnull@localhost>
2011-04-22 12:58:08 -0400

    added suppression for empty interface

--- EPS.Web.Authentication/Basic/Configuration/IBasicAuthenticatorConfiguration.cs
+++ EPS.Web.Authentication/Basic/Configuration/IBasicAuthenticatorConfiguration.cs
@@ -1,11 +1,13 @@
 using System;
 using EPS.Web.Authentication.Configuration;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EPS.Web.Authentication.Basic.Configuration
 {
-    /// <summary>   Interface for basic authentication header inspector configuration element. </summary>
-    /// <remarks>   ebrown, 3/28/2011. </remarks>
-    public interface IBasicAuthenticatorConfiguration :
-            IAuthenticatorConfiguration
-    { }
+	/// <summary>   Interface for basic authentication header inspector configuration element. </summary>
+	/// <remarks>   ebrown, 3/28/2011. </remarks>
+	[SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = "We might later expand this interface in addition to the fact that it fits with our architecture")]
+	public interface IBasicAuthenticatorConfiguration :
+			IAuthenticatorConfiguration
+	{ }
 }
