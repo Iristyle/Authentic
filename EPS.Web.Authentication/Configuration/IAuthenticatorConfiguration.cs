d668e6558958b4486a36c8b3d993d708390d7c9f
ebrown <devnull@localhost>
2011-04-23 10:56:49 -0400

    - made nonceValidDuration an optional value (with default to 5 mins) - added suppression for unrelated interfaces

--- EPS.Web.Authentication/Configuration/IAuthenticatorConfiguration.cs
+++ EPS.Web.Authentication/Configuration/IAuthenticatorConfiguration.cs
@@ -1,42 +1,43 @@
-using System;
+using System.Diagnostics.CodeAnalysis;
 using EPS.Web.Abstractions;
 using EPS.Web.Authentication.Abstractions;
 
 namespace EPS.Web.Authentication.Configuration
 {
-    /// <summary>   Interface for http context inspecting authenticator configuration element. </summary>
-    /// <remarks>   ebrown, 3/28/2011. </remarks>
-    public interface IAuthenticatorConfiguration
-    {
-        /// <summary>   Gets or sets the name of the role provider used to validate the principal. </summary>
-        /// <value> The name of the role provider. </value>
-        string RoleProviderName { get; set; }
-        
-        /// <summary>   Gets or sets the human-friendly name / key for this inspector. </summary>
-        /// <value> The name. </value>
-        string Name { get; set; }
-        
-        /// <summary>   Gets or sets the authenticator instance. </summary>
-        /// <value> The authenticator. </value>
-        IAuthenticator Authenticator { get; }
-        
-        /// <summary>   Gets or sets a value indicating whether the require SSL. </summary>
-        /// <value> true if require SSL, false if not. </value>
-        bool RequireSsl { get; set; }
-        
-        /// <summary>   
-        /// Get or sets the name of the MembershipProvider to be used.  By default no membership provider is used as it may be just as costly as
-        /// extracting a principal. If a simpler membership provider exists that can provide a faster validation of user credentials than a full
-        /// IPrincipal extraction, then it makes sense to use a membershipProvider.  Specify 'default' to use the default configured
-        /// MembershipProvider for the system. 
-        /// </summary>
-        /// <value> The name of the provider. </value>
-        string ProviderName { get; set; }
-        
-        /// <summary>   Gets or sets the principal builder instance. </summary>
-        /// <value> 
-        /// The builder instance.
-        /// </value>
-        IPrincipalBuilder PrincipalBuilder { get; }
-    }
+	/// <summary>   Interface for http context inspecting authenticator configuration element. </summary>
+	/// <remarks>   ebrown, 3/28/2011. </remarks>
+	[SuppressMessage("Gendarme.Rules.Design", "ConsiderAddingInterfaceRule", Justification = "IFailureHandlerConfiguration and IAuthenticatorConfiguration do not share commonality")]
+	public interface IAuthenticatorConfiguration
+	{
+		/// <summary>   Gets or sets the name of the role provider used to validate the principal. </summary>
+		/// <value> The name of the role provider. </value>
+		string RoleProviderName { get; set; }
+
+		/// <summary>   Gets or sets the human-friendly name / key for this inspector. </summary>
+		/// <value> The name. </value>
+		string Name { get; set; }
+
+		/// <summary>   Gets or sets the authenticator instance. </summary>
+		/// <value> The authenticator. </value>
+		IAuthenticator Authenticator { get; }
+
+		/// <summary>   Gets or sets a value indicating whether the require SSL. </summary>
+		/// <value> true if require SSL, false if not. </value>
+		bool RequireSsl { get; set; }
+
+		/// <summary>   
+		/// Get or sets the name of the MembershipProvider to be used.  By default no membership provider is used as it may be just as costly as
+		/// extracting a principal. If a simpler membership provider exists that can provide a faster validation of user credentials than a full
+		/// IPrincipal extraction, then it makes sense to use a membershipProvider.  Specify 'default' to use the default configured
+		/// MembershipProvider for the system. 
+		/// </summary>
+		/// <value> The name of the provider. </value>
+		string ProviderName { get; set; }
+
+		/// <summary>   Gets or sets the principal builder instance. </summary>
+		/// <value> 
+		/// The builder instance.
+		/// </value>
+		IPrincipalBuilder PrincipalBuilder { get; }
+	}
 }
--- EPS.Web.Authentication/Digest/Configuration/DigestAuthenticatorConfiguration.cs
+++ EPS.Web.Authentication/Digest/Configuration/DigestAuthenticatorConfiguration.cs
@@ -10,29 +10,34 @@ namespace EPS.Web.Authentication.Digest.Configuration
 	public class DigestAuthenticatorConfiguration :
 		AuthenticatorConfiguration, IDigestAuthenticatorConfiguration
 	{
+		private TimeSpan _nonceValidDuration = new TimeSpan(0, 5, 0);
+
 		/// <summary>
 		/// Initializes a new instance of the DigestAuthenticatorConfiguration class.
 		/// </summary>
 		public DigestAuthenticatorConfiguration(string name, IAuthenticator authenticator,
-			IPrincipalBuilder principalBuilder, string realm, string privateKey, TimeSpan nonceValidDuration) :
+			IPrincipalBuilder principalBuilder, string realm, string privateKey) :
 			base(name, authenticator, principalBuilder)
 		{
 			//TODO: 4-8-2011 cook up an AbstractValidator class that verifies this goop
 			Realm = realm;
 			PrivateKey = privateKey;
-			NonceValidDuration = nonceValidDuration;
 		}
 
 		/// <summary>   Gets or sets the realm of the digest response on an outgoing 401 challenge. </summary>
 		/// <value> The realm. </value>
-		public string Realm { get; set; }
+		public string Realm { get; private set; }
 
 		/// <summary>   Gets or sets the Private Key value used when generating nonce values. </summary>
 		/// <value> The private key. </value>
-		public string PrivateKey { get; set; }
+		public string PrivateKey { get; private set; }
 
 		/// <summary>   Gets or sets the timespan that a nonce is valid. </summary>
 		/// <value> The duration that the nonce is valid for. </value>
-		public TimeSpan NonceValidDuration { get; set; }
+		public TimeSpan NonceValidDuration
+		{
+			get { return _nonceValidDuration; }
+			set { _nonceValidDuration = value; }
+		}
 	}
 }
