516de25fa5a0eb748d5e1ca7d2cf4799885f49a0
ebrown <devnull@localhost>
2011-06-10 13:58:20 -0400

    removed event based wackiness for configuring HttpAuthenticationModule replaced instead with a custom Func<> that users must implement

--- EPS.Web.Authentication/HttpAuthenticationModule.cs
+++ EPS.Web.Authentication/HttpAuthenticationModule.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using Common.Logging;
-using EPS.Utility;
 using EPS.Web.Abstractions;
 using EPS.Web.Authentication.Abstractions;
 using EPS.Web.Authentication.Configuration;
@@ -18,32 +17,41 @@ namespace EPS.Web.Authentication
 	/// This is the meat of the authentication system.  This class is the top-level HttpModule responsible for delegating inspection of
 	/// HttpContext to any plugins that follow our simple design guidelines. Inspectors are prioritized on a first-come, first-serve basis
 	/// into the OnPostAuthenticateRequest handler. If no suitable inspectors (that return a valid IPrincipal) are found, then the system
-	/// fails over to the configured failure handler. 
+	/// fails over to the configured failure handler. The static 'Configure' should be implemented near the root of a given web application,
+	/// to configure authenticators through code.
 	/// </summary>
 	/// <remarks>   ebrown, 1/3/2011. </remarks>
 	public class HttpAuthenticationModule : HttpModuleBase
 	{
 		private static readonly ILog log = LogManager.GetCurrentClassLogger();
 		private IModuleConfiguration configuration;
+		//TODO: 6-10-2011 -- we should provide a similar hook for the standard .config parsers to publish this information up to the module level
+		private static Func<HttpAuthenticationModule, IModuleConfiguration> _configure;
 
-		/// <summary> Event queue for all listeners interested in Configure events. </summary>
-		public static event EventHandler<HttpAuthenticationModuleConfigureEventArgs> Configure;
+		/// <summary>	
+		/// A user defined configuration function so that the module may be configured in code.  This should be implemented as close to the root
+		/// of the application as possible, for instance in global.asax.cs Application_Start or similar.  If a request comes in before this is
+		/// setup, an Exception will be thrown. 
+		/// </summary>
+		/// <value>	A custom method responsible for returning configuration information for the . </value>
+		public static Func<HttpAuthenticationModule, IModuleConfiguration> Configure
+		{
+			get { return _configure; }
+			set 
+			{
+				if (null != _configure)
+				{
+					throw new InvalidOperationException("Only one configuration resolver may be set during the lifetime of the HttpAuthenticationModule");
+				}
 
+				_configure = value; 
+			}
+		}
 		/// <summary>	Registers the module.  This is designed to allow drop-in registration of the HttpModule without touching web.config</summary>
 		/// <remarks>	ebrown, 4/14/2011. </remarks>
 		public static void RegisterModule()
 		{
 			DynamicModuleUtility.RegisterModule(typeof(HttpAuthenticationModule));
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the HttpAuthenticationModule class.  Makes a call to the static Configure EventHandler to 
-		/// configure the instance.
-		/// </summary>
-		public HttpAuthenticationModule()
-		{
-			//allow for code-based configuration
-			Configure.SafeInvoke(this, new HttpAuthenticationModuleConfigureEventArgs(this));
 		}
 
 		/// <summary>   
@@ -58,18 +66,20 @@ namespace EPS.Web.Authentication
 			{
 				if (configuration == null)
 				{
-					throw new InvalidOperationException("Failed to load the header inspector configuration section, either this dependency has not been injected or set manually before using");
+					if (null == _configure)
+					{
+						throw new NotImplementedException("HttpAuthenticationModule.Configure must be set in code to enable authentication");
+					}
+					
+					configuration = _configure(this);
+					
+					if (null == configuration)
+					{
+						throw new InvalidOperationException("Failed to load the header inspector configuration.  HttpAuthenticationModule.Configure has been set, but does not return a valid IModuleConfiguration instance.");
+					}
 				}
 
 				return configuration;
-			}
-			//this property should be inject by our IoC container
-			set
-			{
-				if (null != configuration)
-					throw new InvalidOperationException("Configuration values may only be set once");
-
-				configuration = value;
 			}
 		}
 
