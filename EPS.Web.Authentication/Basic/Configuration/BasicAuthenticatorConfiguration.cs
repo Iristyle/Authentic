dd90530c94934a0a17a44d6fd7c8a58c7eccedc1
ebrown <devnull@localhost>
2011-09-09 11:17:45 -0400

    - added missing BasicAuthenticatorConfiguration since no classes derived from IBasicAuthenticatorConfiguration whoops! - added SimplePrincipalBuilder that allows for a Func<> to define the principal building process rather than implementing a full-blown class derived from IPrincipalBuilder

---
+++ EPS.Web.Authentication/Basic/Configuration/BasicAuthenticatorConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EPS.Web.Abstractions;
+using EPS.Web.Authentication.Abstractions;
+using EPS.Web.Authentication.Configuration;
+
+namespace EPS.Web.Authentication.Basic.Configuration
+{
+	/// <summary>	Basic authenticator configuration.  </summary>
+	/// <remarks>	9/2/2011. </remarks>
+	public class BasicAuthenticatorConfiguration
+		: AuthenticatorConfiguration, IBasicAuthenticatorConfiguration
+	{
+		/// <summary>	Initializes a new instance of the AuthenticatorConfiguration class. </summary>
+		/// <remarks>	9/2/2011. </remarks>
+		/// <param name="name">			   	The name. </param>
+		/// <param name="authenticator">   	The authenticator. </param>
+		/// <param name="principalBuilder">	The principal builder. </param>
+		public BasicAuthenticatorConfiguration(string name, IAuthenticator authenticator, IPrincipalBuilder principalBuilder)
+			: base(name, authenticator, principalBuilder)
+		{ }
+	}
+}
---
+++ EPS.Web.Authentication/SimplePrincipalBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using EPS.Web.Abstractions;
+using EPS.Web.Authentication.Configuration;
+
+namespace EPS.Web.Authentication
+{
+	/// <summary>
+	/// Simple principal builder that doesn't accept configuration and can be used to provide an inline anonymous function to build
+	/// principals.
+	/// </summary>
+	/// <remarks>	9/2/2011. </remarks>
+	public class SimplePrincipalBuilder : IPrincipalBuilder
+	{
+		private readonly Func<HttpContextBase, MembershipUser, string, string, IPrincipal> _constructor;
+
+		/// <summary>	Initializes a new instance of the SimplePrincipalBuilder class. </summary>
+		/// <remarks>	9/2/2011. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when one or more required arguments are null. </exception>
+		/// <param name="constructor">	An inspection function that accepts the context, an optional membership user, username and password in
+		/// 							that order. </param>
+		public SimplePrincipalBuilder(Func<HttpContextBase, MembershipUser, string, string, IPrincipal> constructor)
+		{
+			if (null == constructor) { throw new ArgumentNullException("constructor"); }
+
+			_constructor = constructor;
+		}
+
+		/// <summary>	Gets the human-friendly name. </summary>
+		/// <value>	The name, which is always SimplePrincipalBuilder. </value>
+		public string Name
+		{
+			get { return "SimplePrincipalBuilder"; }
+		}
+
+		/// <summary>
+		/// Gets the configuration defining the <see cref="T:System.Web.Security.MembershipProvider"/> and <see cref="T:
+		/// EPS.Web.Abstractions.IPrincipalBuilderFactory"/> to use.
+		/// </summary>
+		/// <value>	Always returns null. </value>
+		public IAuthenticatorConfiguration Configuration
+		{
+			get { return null; }
+		}
+
+		/// <summary>	Construct an IPrincipal given a username and password / MembershipUser (context thrown in for good measure). </summary>
+		/// <remarks>	9/2/2011. </remarks>
+		/// <param name="context">		 	The context. </param>
+		/// <param name="membershipUser">	The membership user extracted if there was a MembershipProvider specified through configuration.  May
+		/// 								be null. </param>
+		/// <param name="userName">		 	The username. </param>
+		/// <param name="password">		 	The password. </param>
+		/// <returns>	An IPrincipal if the given credentials could be authenticated, otherwise null. </returns>
+		public IPrincipal ConstructPrincipal(HttpContextBase context, MembershipUser membershipUser, string userName, string password)
+		{
+			return _constructor(context, membershipUser, userName, password);
+		}
+	}
+}
